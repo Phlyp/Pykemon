23f8fe19ca70d75741e21cbf6bab10a4d63a0e52
Phlyp <pypruessner@gmail.com>
2022-05-18 01:17:57 +0200

    added basic methods to create and manage team

--- main.py
+++ main.py
@@ -5,15 +5,10 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
 
 
-database.initialise("Data\pokemon.sqlite")
-
-print("Welcome to Pykemon!\n")
-
-
 def game_engine():
     while True:
         print(" 1. Choose your Team \n 2. Fight!\n 3. Exit!")
-        des = input("Please use the Keys 1-3 to choose what to do next!")
+        des = input("Please use the Keys 1-3 + ENTER to choose what to do next! ")
 
         if des == "1":
             choose_team()
@@ -27,7 +22,16 @@ def game_engine():
 
     
 def choose_team():
-    print("Not implemented!")
+    database.deleteTeam()
+    print(" 1. Choose your own Team\n 2. Create a random Team")
+    des = input("Please use the Keys 1-2 + ENTER to choose what to do next! ")
+    if des == "1":
+        print("Not implemented!")
+    elif des == "2":
+        database.createRandomTeam()
+
+    database.listTeam()
+    print("\n") 
 
 def fight_sim():
     print("Not implemented!")
@@ -35,4 +39,7 @@ def fight_sim():
 
 
 if __name__ == "__main__":
+    database.initialise()
+    print("Welcome to Pykemon!\n")
+
     game_engine()
