8a6919c2388546940869b1277c27e4996d6f750f
Phlyp <pypruessner@gmail.com>
2022-05-17 15:57:12 +0200

    super basic main loop

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+import os
 import database
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
+
 
 database.initialise("Data\pokemon.sqlite")
 
-print("Welcome to Pykemon!")
+print("Welcome to Pykemon!\n")
+
+
+def game_engine():
+    while True:
+        print(" 1. Choose your Team \n 2. Fight!\n 3. Exit!")
+        des = input("Please use the Keys 1-3 to choose what to do next!")
+
+        if des == "1":
+            choose_team()
+        elif des == "2":
+            fight_sim()
+        elif des == "3":
+            print("Goodbye!")
+            quit()
+        else:
+            print("invalid input given!")
+
+    
+def choose_team():
+    print("Not implemented!")
+
+def fight_sim():
+    print("Not implemented!")
+
+
+
+if __name__ == "__main__":
+    game_engine()
